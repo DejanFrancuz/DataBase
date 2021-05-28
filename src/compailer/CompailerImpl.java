5e0d67804a6cfef643b83a033261a804b257bd49
Dejan <dfrancuz7620rn@raf.rs>
2021-05-28 17:51:22 +0200

    ubacen orderByDesc

--- src/compailer/CompailerImpl.java
+++ src/compailer/CompailerImpl.java
@@ -6,37 +6,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CompailerImpl implements Compailer{
-
+    int order=0;
     MainFrame mf=MainFrame.getInstance();
     String string = "";
     //var query = new Query("Departments").OrderBy("manager_id")
     @Override
     public void compaile(String s){
-        Map<String,String[]> map = new HashMap<>();
-        map.put("Query",null);
-        map.put("Select",null);
-        map.put("OrderBy",null);
-        map.put("Join",null);
-        map.put("On",null);
-        map.put("Where",null);
-        map.put("OrWhere",null);
-        map.put("AndWhere",null);
-        map.put("WhereBetween",null);
-        map.put("WhereIn",null);
-        map.put("ParametarList",null);
-        map.put("WhereEndsWith",null);
-        map.put("WhereStartsWith",null);
-        map.put("WhereCounts",null);
-        map.put("Avg",null);
-        map.put("Count",null);
-        map.put("Min",null);
-        map.put("Max",null);
-        map.put("GroupBy",null);
-        map.put("Having",null);
-        map.put("AndHaving",null);
-        map.put("OrHaving",null);
-        map.put("WhereInQ",null);
-        map.put("WhereEqQ",null);
         String[] array=s.split("\\.");
         String star="*";
         for(int i=0;i<array.length;i++){
@@ -65,8 +40,19 @@ public class CompailerImpl implements Compailer{
             }
         }
         for(int i=0;i<array.length;i++){
-            if(array[i].contains("OrderBy")||array[i].contains("OrderByDesc"))orderBy(array[i]);
+            if(array[i].contains("OrderByDesc")){
+                orderByDesc(array[i]);
+                order=1;
+                break;
+            }
         }
+        for(int i=0;i<array.length;i++){
+            if(array[i].contains("OrderBy")&&(order==0)){
+                orderBy(array[i]);
+                break;
+            }
+        }
+        order=0;
           mf.getAppCore().readDataFromTable(string);
         System.out.println(string);
 
@@ -163,4 +149,10 @@ public class CompailerImpl implements Compailer{
             string= string+","+array[i];
         }else
             string+="*";}
+    public void orderByDesc(String s){
+        String[] array = s.split("\"");
+        string+=" ORDER BY ";
+        string+=array[1];
+        string+=" DESC ";
+    }
 }
