356b8a962bcb501f7b405e002e0ac42c2435b69a
Dejan <dfrancuz7620rn@raf.rs>
2021-05-31 13:10:31 +0200

    implementirani svi upiti

--- src/compailer/CompailerImpl.java
+++ src/compailer/CompailerImpl.java
@@ -26,6 +26,24 @@ public class CompailerImpl implements Compailer{
             }
         }
         for(int i=0;i<array.length;i++){
+            if(array[i].contains("Count")){
+                count(array[i]);
+                break;
+            }
+        }
+        for(int i=0;i<array.length;i++){
+            if(array[i].contains("Max")){
+                max(array[i]);
+                break;
+            }
+        }
+        for(int i=0;i<array.length;i++){
+            if(array[i].contains("Min")){
+                min(array[i]);
+                break;
+            }
+        }
+        for(int i=0;i<array.length;i++){
             if(array[i].contains("Query"))query(array[i]);
         }
         for(int i=0;i<array.length;i++){
@@ -78,6 +96,30 @@ public class CompailerImpl implements Compailer{
             }
         }
         for(int i=0;i<array.length;i++){
+            if(array[i].contains("GroupBy")){
+                groupBy(array[i]);
+                break;
+            }
+        }
+        for(int i=0;i<array.length;i++){
+            if(array[i].contains("Having")&&array[i].charAt(0)=='H'){
+                having(array[i]);
+                break;
+            }
+        }
+        for(int i=0;i<array.length;i++){
+            if(array[i].contains("AndHaving")&&array[i].charAt(0)=='H'){
+                andHaving(array[i]);
+                break;
+            }
+        }
+        for(int i=0;i<array.length;i++){
+            if(array[i].contains("OrHaving")){
+                orHaving(array[i]);
+                break;
+            }
+        }
+        for(int i=0;i<array.length;i++){
             if(array[i].contains("OrderByDesc")){
                 orderByDesc(array[i]);
                 order=1;
@@ -95,19 +137,161 @@ public class CompailerImpl implements Compailer{
         System.out.println(string);
 
     }
-    public void avg(String s){
+    public void andHaving(String s){
         String[] array = s.split(",");
-        String[] array1 = array[1].split("\\)"); // prosecnaPlata 0
+        String[] array1=array[0].split("\"");
+        string+=" AND ";
+        //Where("department_id", ">", 50)
+        string+=array1[1]; // dep id
+        string+=" ";
+        String[] array2 = array[1].split("\"");
+        string+=array2[1]; // >
+        string+=" ";
+        if(array[2].contains("\"")){
+            String[] array3 =array[2].split("\"");
+            if(array3[1].contains("%")){
+                String sima="'";
+                sima+=array3[1];
+                sima+="'";
+                string+=sima;
+            }else
+                string+=array3[1];}else{
+            String[] array4 =array[2].split("\\)");
+            string+=array4[0]; // 50
+        }
+        //string.replaceAll("\\)"," ");
+    }
+    public void orHaving(String s){
+        String[] array = s.split(",");
+        String[] array1=array[0].split("\"");
+        string+=" OR ";
+        //Where("department_id", ">", 50)
+        string+=array1[1]; // dep id
+        string+=" ";
+        String[] array2 = array[1].split("\"");
+        string+=array2[1]; // >
+        string+=" ";
+        if(array[2].contains("\"")){
+            String[] array3 =array[2].split("\"");
+            if(array3[1].contains("%")){
+                String sima="'";
+                sima+=array3[1];
+                sima+="'";
+                string+=sima;
+            }else
+                string+=array3[1];}else{
+            String[] array4 =array[2].split("\\)");
+            string+=array4[0]; // 50
+        }
+        //string.replaceAll("\\)"," ");
+    }
+    public void having(String s){ // having("salary",">","500")
+        String[] array = s.split(",");
+        String[] array1=array[0].split("\"");
+        string+=" HAVING ";
+        //Where("department_id", ">", 50)
+        string+=array1[1]; // dep id
+        string+=" ";
+        String[] array2 = array[1].split("\"");
+        string+=array2[1]; // >
+        string+=" ";
+        if(array[2].contains("\"")){
+            String[] array3 =array[2].split("\"");
+            if(array3[1].contains("%")){
+                String sima="'";
+                sima+=array3[1];
+                sima+="'";
+                string+=sima;
+            }else
+                string+=array3[1];}else{
+            String[] array4 =array[2].split("\\)");
+            string+=array4[0]; // 50
+        }
+        //string.replaceAll("\\)"," ");
+    }
+    public void max(String s){
+        String[] array = s.split(",");
+        String[] array2 = array[0].split("\"");
+        String n ="";
+        n+=" MAX(";
+        n+=array2[1];
+        n+=")";
+        if(array.length==2){
+            String[] array1 = array[1].split("\\)");
+            String[] array3 = array1[0].split("\"");
+            n+=" AS ";
+            n += "\""+array3[1]+"\"";
+            string = string.replaceAll(array3[1],n);}else{
+            String m =",";
+            m+=n;
+            string+=m;
+        }
+        //  string+=array1[0];
+
+    }
+    public void min(String s){
+        String[] array = s.split(",");
+        String[] array2 = array[0].split("\"");
+        String n ="";
+        n+=" MIN(";
+        n+=array2[1]; // salary
+        n+=")";
+        if(array.length==2){
+            String[] array1 = array[1].split("\\)");
+            String[] array3 = array1[0].split("\"");
+            n+=" AS ";
+            n += "\""+array3[1]+"\"";
+            string = string.replaceAll(array3[1],n);}else{
+            String m =",";
+            m+=n;
+            string+=m;
+        }
+
+    }
+    public void count(String s){ // Avg("salary","prosecnaPlata") = avg(salary) as “prosecnaPlata”
+        String[] array = s.split(",");
         String[] array2 = array[0].split("\""); // salary 1
-        String[] array3 = array1[0].split("\"");
-        string+=" AVG (";
-        string+=array2[1];
-        string+=")";
-        string+=" AS ";
-        String n = "["+array3[1]+"]";
-        string+=n;
+        String n ="";
+        n+=" COUNT(";
+        n+=array2[1]; // salary
+        n+=")";
+        if(array.length==2){
+            String[] array1 = array[1].split("\\)"); // prosecnaPlata 0
+            String[] array3 = array1[0].split("\"");
+            n+=" AS ";
+            n += "\""+array3[1]+"\"";
+            string = string.replaceAll(array3[1],n);}else{
+            String m =",";
+            m+=n;
+            string+=m;
+        }
+        //  string+=array1[0];
+
+    }
+    public void avg(String s){ // Avg("salary","prosecnaPlata") = avg(salary) as “prosecnaPlata”
+        String[] array = s.split(",");
+        String[] array2 = array[0].split("\""); // salary 1
+        String n ="";
+        n+=" AVG(";
+        n+=array2[1]; // salary
+        n+=")";
+        if(array.length==2){
+            String[] array1 = array[1].split("\\)"); // prosecnaPlata 0
+            String[] array3 = array1[0].split("\"");
+        n+=" AS ";
+        n += "\""+array3[1]+"\"";
+        string = string.replaceAll(array3[1],n);}else{
+            String m =",";
+            m+=n;
+            string+=m;
+        }
       //  string+=array1[0];
 
+    }
+    public void groupBy(String s){
+        String[] array=s.split("\"");
+        string+=" GROUP BY ";
+        string+=array[1];
     }
     public void join(String s){
         String[] array=s.split("\"");
